8707cb91fd13562275cc5f986379899b75a6e48d
Chris Natale <chris.natale@ookla.com>
2017-04-12 12:19:41 -0700

    add knex util test

--- test/server/db/knex.spec.js
+++
@@ -1,1 +0,0 @@
-// TODO: write tests to test connection with database, that it returns expected data
---
+++ test/server/unit/db/knex.spec.js
@@ -0,0 +1,22 @@
+// TODO: write tests to test connection with database, that it returns expected data
+const chai = require('chai');
+const _ = require('lodash');
+const chaiAsPromised = require('chai-as-promised');
+const knex = require('../../../../server/db/knex');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('knex util', () => {
+  it('should connect to db and get sample data', (done) => {
+    knex('test')
+      .then((results) => {
+        expect(results).to.be.instanceof(Array);
+        expect(results.length).to.be.above(0);
+        _.forEach(results, function(result) {
+          expect(result).to.have.property('a').that.is.a('string');
+        });
+
+        done();
+      });
+  });
+});
