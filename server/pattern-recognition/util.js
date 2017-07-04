620588df37b14fe5c99679c715e0f23387b195ef
Chris Natale <chris.natale@ookla.com>
2017-07-03 23:49:55 -0700

    refactor cartesianProduct method, add documentation

--- server/pattern-recognition/util.js
+++ server/pattern-recognition/util.js
@@ -1,6 +1,25 @@
-// TODO: write tests for multiple dimensions/length combos
+/**
+  @param sets: an n element array, with each element an array of possible
+    values for the i'th component.
+    ex: [['a', 'b', 'c'], [1, 2, 3], ['x', 'y', 'z']]
 
-exports.cartesianProduct = (sets, outputList = [], currentIndices, depth = 0) => {
+  @param currentIndices: the current index value for all n components
+
+  @param depth: how 'deep' the algorithm has descended, in terms of dimension 0
+    of the sets array.
+    ex: a value of 2 would relate to the the array ['x', 'y', 'z'] in the example
+      above.
+
+  @returns an array of all possible signal combinations
+  example usage:
+    cartesianProduct([['a', 'b'], [1, 2]])
+
+    // outputs: ['a_1', 'a_2', 'b_1', 'b_2']
+*/
+
+exports.cartesianProduct = (sets, currentIndices, depth = 0) => {
+  let outputList = [];
+
   // if no current indices, initialize all to 0.
   if (!currentIndices) {
     currentIndices = sets.map(() => {
@@ -14,7 +33,7 @@ exports.cartesianProduct = (sets, outputList = [], currentIndices, depth = 0) =>
     // recursively descend to length-1 elements in sets every increment
     if (depth < sets.length - 1) {
       outputList = outputList.concat(
-        exports.cartesianProduct(sets, [], currentIndices, depth + 1)
+        exports.cartesianProduct(sets, currentIndices, depth + 1)
       );
     } else {
       // add a combination when at deepest depth
