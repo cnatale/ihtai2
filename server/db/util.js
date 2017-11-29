1200212c6a744685ac0ee789275e6f9ce07952ef
Chris Natale <chris.natale@ookla.com>
2017-11-29 09:29:31 -0800

    add ability to drop tables to emptyDb method

--- server/db/util.js
+++ server/db/util.js
@@ -2,8 +2,11 @@ const knexCleaner = require('knex-cleaner');
 const knex = require('knex');
 const config = require('config');
 
-exports.emptyDb = () => {
+exports.emptyDb = (dropTables = false) => {
+  const options = {};
+  if (dropTables) { options.mode = 'delete'; }
   return knexCleaner.clean(
-    knex({ client: config.db.type, connection: config.get(config.db.type) })
+    knex({ client: config.db.type, connection: config.get(config.db.type) }),
+    options
   );
 };
