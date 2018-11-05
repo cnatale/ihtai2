a9fe1dafd936b944501bf9a30f29eeb590652741
Chris Natale <chris.natale@ookla.com>
2018-11-05 07:45:00 -0800

    update automated test to puppeteer

--- automated_browser_scripts/test.js
+++ automated_browser_scripts/test.js
@@ -1,18 +1,5 @@
-const webdriverio = require('webdriverio');
+const puppeteer = require('puppeteer');
 const _ = require('lodash');
-const options = {
-  desiredCapabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-      args: [
-        '--disable-background-timer-throttling',
-        '--pause-background-tabs',
-        '--headless'
-      ]
-    }
-  }
-};
-const client = webdriverio.remote(options);
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 console.log('starting end to end test with the following arguments:');
@@ -21,82 +8,78 @@ console.log(argv);
 let count = 0;
 const numberOfTestsToRun = 1200;
 
-
 // build filename
 let fileName = './data/';
+console.log(argv);
 _.forEach(argv, (value, key) => {
   if (key !== '_') {
     fileName = `${fileName}_${key}_${value}`;
   }
 });
 fileName = fileName + '.csv';
+console.log(`Output filename: ${fileName}`);
 
-function waitLoop() {
+async function waitLoop(page) {
   let avgScore, pctTimeInTargetArea;
 
-  client.getText('#statusBox').then((value) => {
-    if (value === 'Test Complete') {
-      Promise.all([client.getText('#avgScore'), client.getText('#pctTimeInTargetArea')]).then((values) => {
-        // append all env vars as csv's
-        let output = '';
-        _.forEach(argv, (value, key) => {
-          if(key === 'scoreTimesteps') {
-            value = `"${value}"`;
-          }
+  const statusBoxContent  = await page.evaluate(() => document.querySelector('#statusBox').innerHTML);
+  // const ballx = await page.evaluate(() => sphere.position.x);
+  // const bally = await page.evaluate(() => sphere.position.y);
+  // const ballz = await page.evaluate(() => sphere.position.z);
+  // console.log(`x: ${ballx} y: ${bally} z: ${ballz}`)
 
-          if (key !== '_') {
-            output = output + value + ',';
-          }
-        });
-        output = output + `${values[0]},${values[1]}\n`;
+  if (statusBoxContent === 'Test Complete') {
+    avgScore = await page.evaluate(() => document.querySelector('#avgScore').innerText);
+    pctTimeInTargetArea = await page.evaluate(() => document.querySelector('#pctTimeInTargetArea').innerText);
 
-        fs.appendFile(fileName, output, function(err) {
-          if (err) {
-            console.log(err);
-          }
+    // append all env vars as csv's
+    let output = '';
+    _.forEach(argv, (value, key) => {
+      if(key === 'scoreTimesteps') {
+        value = `"${value}"`;
+      }
 
-          console.log(`test ${count} complete`);
-          count++;
-          client.refresh();
-        });
-      });
-    }
-  });
+      if (key !== '_') {
+        output = output + value + ',';
+      }
+    });
+    output = output + `${avgScore},${pctTimeInTargetArea}\n`;
 
+    fs.appendFile(fileName, output, function(err) {
+      if (err) {
+        console.log(err);
+      }
+
+      console.log(`test ${count} complete`);
+      count++;
+      page.reload();
+    });
+  }
 
   if (count < numberOfTestsToRun) {
-    setTimeout(waitLoop, 1500);
+    setTimeout(() => {
+      waitLoop(page);
+    }, 1500);
   } else {
     console.log('*** all tests complete! ***')
-    // client.end();
   }
 }
 
-client
-  .init()
-  .url('http://localhost:3900/balldemo_multistep.html')
-  .then(() => {
-    let output = '\n\n';
-    _.forEach(argv, (value, key) => {
-      if (key !== '_') {
-        output = output + key + ',';
-      }
-    });
-    output = output + 'Average Score,Percentage Time in Target Area\n';
-
-    // adds column titles for csv rows
-    // return new Promise((resolve, reject) => {
-    //   fs.appendFile(fileName, output, function(err) {
-    //         if (err) {
-    //           console.log(err);
-    //           return reject();
-    //         }
-
-    //         return resolve();
-    //       });
-    // });
-
-  })
-  .then(() => {
-    setTimeout(waitLoop, 1500);
+(async () => {
+    // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    // headless: true,
+  const browser = await puppeteer.launch({
+    args: [
+        '--window-size=640,480'
+    ]
   });
+  const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
+  // await page.setViewport({ width: 640, height: 480 })
+
+  await page.goto('http://localhost:3900/balldemo_multistep.html');
+
+  setTimeout(() => {
+    waitLoop(page);
+  }, 1500);
+})();
